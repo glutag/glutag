54853f45c0ad13bc2e77ddf3f42473a0b5544a27
Sanket' s Air <sanketsair@Sankets-MacBook-Air.local>
2025-09-29 02:37:20 +0530

    selection sort corrected

--- C_learnings/selection_sort.c
+++ C_learnings/selection_sort.c
@@ -4,22 +4,29 @@
 void selection_sort (int arr[],int n){
     for (int i = 0; i < n-1; i++){
         int minindex = i;
+        printf(" i = %i \n", i);
         for (int j = i+1; j <  n; j++){
             if (arr[j] < arr[minindex])
             {
+                printf("j = %i \n", j);
                 minindex = j;
+                int temp = arr[i];
+                printf("temp = %i \n", temp);
+                arr[i] = arr[minindex];
+                printf("arr[minindex] = %i \n", arr[minindex]);
+                printf("inner lop ends \n");
+                arr[minindex] = temp;
             }
-            int temp = arr[i];
-            arr[i] = arr[minindex];
-            arr[minindex] = temp;
+            
         }
     }
 }
 
 void printarray(int arr[], int n){
     for (int i=0; i < n; i++){
-        printf("%d ", arr[i]);
+        printf("%d,", arr[i]);
     }
+    printf("\n");
 }
 
 int main(void) {
